16039f8e8744d0cfdb14a3c781fe3af12346450b
iyappanapps <hpadvaith@gmail.com>
2024-02-26 15:55:12 +0530

    testing

--- indoor-navigation/src/app/app.component.ts
+++ indoor-navigation/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indoor-navigation-ting';
+  title = 'indoor-navigation-testing';
 }
